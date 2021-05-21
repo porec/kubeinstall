b6191190058672b4f0a3303f5392657ad0b87974
porec <dzmitry.reshytnik@gmail.com>
2021-05-21 18:20:16 +0200

    updating install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
-echo "Updating Server"
+green=`tput setaf 2`
+reset=`tput sgr0` 
+echo "${green}Updating Server${reset}"
 dnf -y upgrade
-echo "Disabling SELinux enforcement"
+echo "${green}Disabling SELinux enforcement${reset}"
+sleep 1s
 setenforce 0
 sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
-echo "Enabling transparent masquerading and facilitate Virtual Extensible LAN (VxLAN) traffic for communication between Kubernetes pods across the cluster"
+sleep 1s
+echo "${green}Enabling transparent masquerading and facilitate Virtual Extensible LAN (VxLAN) traffic for communication between Kubernetes pods across the cluster${reset}"
 modprobe br_netfilter
-echo "Enabling IP masquerade at the firewall"
+sleep 1s
+echo "${green}Enabling IP masquerade at the firewall${reset}"
 firewall-cmd --add-masquerade --permanent
-echo "Reloading firewall"
+sleep 1s
+echo "${green}Reloading firewall${reset}"
 firewall-cmd --reload
-
+sleep 1s
+echo "${green}Set bridged packets to traverse iptables rules${reset}"
+cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sleep 1s
+echo "${green}Loading the new rules${reset}"
+sysctl --system
+echo "${green}New rules are loaded${reset}"
+sleep 1s
+echo "${green}Disabling all memory swaps to increase performance${reset}"
+swapoff -a
+sed -i 's|/dev/mapper/cs-swap|#/dev/mapper/cs-swap|g' /etc/fstab
+sleep 1s
