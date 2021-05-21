d531261eebf7af5978d4f33e58a1ea8b908a69e9
dreshytnik <32958664+dreshytnik@users.noreply.github.com>
2021-05-21 17:06:21 +0200

    updating install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+echo "Updating Server"
 dnf -y upgrade
+echo "Disabling SELinux enforcement"
 setenforce 0
 sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+echo "Enabling transparent masquerading and facilitate Virtual Extensible LAN (VxLAN) traffic for communication between Kubernetes pods across the cluster"
 modprobe br_netfilter
+echo "Enabling IP masquerade at the firewall"
 firewall-cmd --add-masquerade --permanent
+echo "Reloading firewall"
 firewall-cmd --reload
 
