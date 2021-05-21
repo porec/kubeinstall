59498f2ceb200655c1934e109104a4649d2c5e1d
dreshytnik <32958664+dreshytnik@users.noreply.github.com>
2021-05-21 16:43:01 +0200

    adding install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+dnf -y upgrade
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+modprobe br_netfilter
+firewall-cmd --add-masquerade --permanent
+firewall-cmd --reload
+
